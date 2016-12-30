aab3dd2d3c663381081a9cd073d3aade464a8eb3
aashishpoudel <poulaashish@yahoo.com>
2016-12-29 21:29:35 -0800

    12/28/2016 - Made 1st version of code to visually see Dictionary 12/29/2016 - Committing Python code for rename_file.py. We can use command line argument now.

--- rename_files.py
+++ rename_files.py
@@ -1,6 +1,24 @@
-import re
+from os import listdir, getcwd, rename
+from os.path import isfile, join
+import os
+import sys
 
-print("Muna Khatiwada")
-print("Aashish Poudel")
-print("We are couples")
-print("THis is just for learning how to push in GITHUB")
+
+current_word = sys.argv[1]
+new_word = sys.argv[2]
+
+my_cwd = getcwd()
+#listdir includes both files and directories
+onlyfiles = [f for f in listdir(my_cwd) if isfile(join(my_cwd, f))]
+
+
+for file in onlyfiles:
+    print(str(file))
+    if current_word in file:
+        old_filename = file
+        part1, part2 = old_filename.split(current_word)
+        new_filename = part1 + new_word + part2
+        print("renaming file " + str(old_filename) + " to " + str(new_filename))
+        os.rename(old_filename, new_filename)
+        print("Now the list of files are:")
+        print(os.listdir(my_cwd))
